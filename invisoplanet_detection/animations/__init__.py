7c84e280c36e4c72611493fefdb1f30be9594661
MDAddario <michael.lindner.daddario@gmail.com>
2020-04-03 16:14:09 -0400

    Creates animations submodule

---
+++ invisoplanet_detection/animations/__init__.py
@@ -0,0 +1,3 @@
+# Import all the components of the animations submodule
+# If new files are added to the directory, they must be added here
+from .example import *
---
+++ invisoplanet_detection/animations/example.py
@@ -0,0 +1,14 @@
+def animations_print():
+	print(
+		"This is an example function call from the animations package.\n"
+		"Apparently if you want to print over multiple lines you can format like this.\n"
+		"Happy animations!"
+	)
+
+
+def another_function():
+	pass
+
+# Do this ^ when you want your code to run without actually
+# having to write the body of the function yet. This also applies
+# to any block of code, such as if statements and for loops
