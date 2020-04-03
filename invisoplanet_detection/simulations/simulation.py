f11a7124cad59e07a252e04c7c4183c17c0e6251
Delaney Dunne <delaney.dunne@mail.mcgill.ca>
2020-04-03 19:50:18 -0400

    removed 'Vector' class, moved operations into body class (it was clunky and annoying)

--- invisoplanet_detection/simulations/simulation.py
+++ invisoplanet_detection/simulations/simulation.py
@@ -3,55 +3,30 @@ import numpy as np
 # define classes
 
 
-class Vector:
-
-    # construct
-    def __init__(self, x, y):
-
-        # store the instance attributes
-        self.x = x
-        self.y = y
-        self.vector = np.array((x, y))
-
-    # return the scalar distance between the vector and another
-    def scalardistance(self, vec2):
-
-        diff = self.vector - vec2.vector
-        return np.sqrt(np.sum(diff ** 2))
-
-    # change the origin of the coordinate system in which the vector is defined
-    # newcenter should be a member of the vector class - vector pointing to origin of the
-    # new coordinate system from the origin of the old system
-    def recenter(self, newcenter):
-
-        return self.vector - newcenter.vector
-
-
-
-
-
 class Body:
-
     # units involved
-    m_earth = 5.972e24 # kg
-    AU = 1.495979e11 # m
+    m_earth = 5.972e24  # kg
+    AU = 1.495979e11  # m
     # gravitational constant in these units
     G = 6.67408e-11 * AU ** 3 / m_earth
 
     # construct
     def __init__(self, q, v, m):
-
-        # store the instance attributes (q and v should be vectors)
+        # store the instance attributes (q and v should be vectors stored as np arrays)
         self.position = q
         self.velocity = v
         self.mass = m
 
+    # find the magnitude of the separation between two bodies
+    def scalardistance(self, body2):
+        diff = self.position - body2.position
+
+        return np.sqrt(np.sum(diff ** 2))
 
     # find the pair gravitational force acting on this body from a single other body
-    def pairforce(self, b2):
-
-        F = Body.G * self.mass * b2.mass * (self.position.vector - b2.position.vector)
-        F = F / self.position.scalardistance(b2.position) ** 3
+    def pairforce(self, body2):
+        F = Body.G * self.mass * body2.mass * (self.position - body2.position)
+        F = F / self.scalardistance(body2) ** 3
         return F
 
     # find the total force acting on this body from all other bodies
@@ -62,22 +37,27 @@ class Body:
 class PhaseSpace:
 
     # construct
-    def __init__(self):
-
+    def __init__(self, bodies, t):
         # store the instance attributes
-        pass
+        # bodies works just like any other list - it can be indexed from zero
+        self.bodies = bodies
+        self.time = t
 
     # find the center of mass of the system at the given instant in time
-    def __findCoM__(self):
+    def findCoM(self):
         pass
 
     # redefine all position and velocity coordinates for all bodies in the system in terms of the
     # center of mass
-    def __CoMrecenter__(self):
+    def CoMrecenter(self):
         pass
 
     # calculate the matrix of all pair forces for the system at this instant
-    def __forcematrix__(self):
+    def forcematrix(self):
+        pass
+
+    # print the position and time information to an external file
+    def printphasespace(self):
         pass
 
 
