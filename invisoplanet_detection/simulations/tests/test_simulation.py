d8ea296c1d9fce828306a9498042c04b47a9d0a4
Delaney Dunne <delaney.dunne@mail.mcgill.ca>
2020-04-05 13:28:27 -0400

    added unit test file to simulation folder

---
+++ invisoplanet_detection/simulations/tests/test_simulation.py
@@ -0,0 +1,14 @@
+import unittest
+import nose.tools as nt
+import numpy as np
+from simulation import *
+
+
+class TestSimulation(eunittest.TestCase):
+
+    def test_something(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- main.py
+++ main.py
@@ -10,24 +10,6 @@ if __name__ == "__main__":
 	>>>> position_data = [[1, 0, 0], [1, 1, 0], [1, 2, 0], [1, 3, 0]]
 	"""
 
-	# Create sample position data
-	parameter = np.linspace(0, 1, num=120)
-	p1_x_pos = 10 * np.cos(2 * np.pi * parameter - np.pi / 4) - 5
-	p1_y_pos = 10 * np.sin(2 * np.pi * parameter - np.pi / 4) - 5
-	p1_z_pos = np.zeros(len(parameter))
-
-	p1_pos = []
-	for x, y, z in zip(p1_x_pos, p1_y_pos, p1_z_pos):
-		p1_pos.append([x, y, z])
-
-	p2_x_pos = -10 * np.sin(2 * np.pi * parameter + np.pi) + 5
-	p2_y_pos = -10 * np.cos(2 * np.pi * parameter + np.pi) + 5
-	p2_z_pos = np.zeros(len(parameter))
-
-	p2_pos = []
-	for x, y, z in zip(p2_x_pos, p2_y_pos, p2_z_pos):
-		p2_pos.append([x, y, z])
-
 	# Create sample masses
 	p1_mass = 4
 	p2_mass = p1_mass
