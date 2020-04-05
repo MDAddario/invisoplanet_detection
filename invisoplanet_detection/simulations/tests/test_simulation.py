673908c4bc3dd55cb551b7fe94207635eb26b8bb
Delaney Dunne <delaney.dunne@mail.mcgill.ca>
2020-04-05 15:23:09 -0400

    added some unit tests for iterate function

--- invisoplanet_detection/simulations/tests/test_simulation.py
+++ invisoplanet_detection/simulations/tests/test_simulation.py
@@ -44,10 +44,16 @@ class TestSimulation():
 
         space, outfile = initialize(testfile, testout)
 
-        # make sure information is stored in every phase space coordinate and mass
         nt.assert_equal(len(space.bodies), 2)
 
         x, v, m = space.arrayvals()
+
+        # check for NaNs
+        nt.assert_equal(np.any(np.isnan(x)), False)
+        nt.assert_equal(np.any(np.isnan(v)), False)
+        nt.assert_equal(np.any(np.isnan(m)), False)
+
+        # check the outputs are the expected shape
         nt.assert_equal(x.shape, (2, 2))
         nt.assert_equal(v.shape, (2, 2))
         nt.assert_equal(m.shape, (2, 1))
@@ -77,6 +83,71 @@ class TestSimulation():
 
 
     def test_iterate(self):
+
+        # setup ***
+        data = {}
+        data['bodies'] = []
+        data['bodies'].append({
+            'mass': '1',
+            "init_pos": {
+                "x": 1,
+                "y": 1
+            },
+            "init_vel": {
+                "x": -1,
+                "y": -1
+            }
+        })
+        data['bodies'].append({
+            'mass': '1',
+            "init_pos": {
+                "x": -1,
+                "y": -11
+            },
+            "init_vel": {
+                "x": -1,
+                "y": -1
+            }
+        })
+
+        testfile = 'invisoplanet_detection/simulation/tests/testdata.json'
+        testout = 'invisoplanet_detection/simulation/tests/testoutput.txt'
+
+        with open(testfile, 'w') as f:
+            json.dump(data, f)
+
+        space_i, outfile = initialize(testfile, testout)
+        space_f = iterate(space_i, 1)
+
+        xi, vi, mi = space_i.arrayvals()
+        xf, vf, mf = space_f.arrayvals()
+
+        # check for NaNs
+        nt.assert_equal(np.any(np.isnan(xf)), False)
+        nt.assert_equal(np.any(np.isnan(vf)), False)
+        nt.assert_equal(np.any(np.isnan(mf)), False)
+
+        # make sure none of the bodies are stacked on top of each other
+        distances = []
+        for bodyi in space_f.bodies:
+            for bodyj in space_f.bodies:
+                distances.extend(bodyi.scalardistance(bodyj))
+        nt.assert_greater(np.all(distances), 0.000001)
+
+        # make sure none of the masses have changed
+        for i in range(2):
+            nt.assert_equal(mi[i], mf[i])
+
+        # check that the CoM has not moved significantly
+        com_i = space_i.findCoM()
+        com_f = space_f.findCoM()
+        for i in range(2):
+            nt.assert_almost_equal(com_i.pos[i], com_f.pos[i])
+            nt.assert_almost_equal(com_i.vel[i], com_f.vel[i])
+        nt.assert_almost_equal(com_i.mass, com_f.mass)
+
+        # check the total energy before and after iterating, make sure it hasn't changed significantly
+        # calculate angular momentum before and after iterating, make sure it hasn't changed significantly
 
         pass
 
