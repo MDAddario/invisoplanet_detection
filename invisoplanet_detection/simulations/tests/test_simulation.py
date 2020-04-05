a9752703315cd55dfdd85b9f96d2a52a1ab0dabf
Delaney Dunne <delaney.dunne@mail.mcgill.ca>
2020-04-05 16:01:47 -0400

    debugged unit tests for initialize and iterate in simulation.py

--- invisoplanet_detection/simulations/tests/test_simulation.py
+++ invisoplanet_detection/simulations/tests/test_simulation.py
@@ -14,7 +14,7 @@ class TestSimulation():
         data = {}
         data['bodies'] = []
         data['bodies'].append({
-            'mass': '1',
+            'mass': 1,
             "init_pos": {
                 "x": 1,
                 "y": 1
@@ -25,10 +25,10 @@ class TestSimulation():
             }
         })
         data['bodies'].append({
-            'mass': '1',
+            'mass': 1,
             "init_pos": {
                 "x": -1,
-                "y": -11
+                "y": -1
             },
             "init_vel": {
                 "x": -1,
@@ -36,8 +36,8 @@ class TestSimulation():
             }
         })
 
-        testfile = 'invisoplanet_detection/simulation/tests/testdata.json'
-        testout = 'invisoplanet_detection/simulation/tests/testoutput.txt'
+        testfile = 'testdata.json'
+        testout = 'testoutput.txt'
 
         with open(testfile, 'w') as f:
             json.dump(data, f)
@@ -58,28 +58,13 @@ class TestSimulation():
         nt.assert_equal(v.shape, (2, 2))
         nt.assert_equal(m.shape, (2, 1))
 
-        # make sure none of the bodies are wildly far away from all of the other ones
-        distances = []
-        for bodyi in space.bodies:
-            for bodyj in space.bodies:
-                distances.extend(bodyi.scalardistance(bodyj))
-        nt.assert_less(np.all(distances), 1000)
-
-        # check the center of mass calculation works for these known values
+        # check the center of mass recentering works for these known values
         com = space.findCoM()
-        for i in range(2):
-            nt.assert_almost_equal(com.pos[i], 0)
-            nt.assert_almost_equal(com.vel[i], -2)
-        nt.assert_almost_equal(com.mass, 2)
-
-        # check that the center of mass recentering works
-        space_centered = space.CoMrecenter()
-
-        com_centered = space_centered.findCoM()
         for i in range(2):
             nt.assert_almost_equal(com.pos[i], 0)
             nt.assert_almost_equal(com.vel[i], 0)
         nt.assert_almost_equal(com.mass, 2)
+
 
 
     def test_iterate(self):
@@ -88,7 +73,7 @@ class TestSimulation():
         data = {}
         data['bodies'] = []
         data['bodies'].append({
-            'mass': '1',
+            'mass': 1,
             "init_pos": {
                 "x": 1,
                 "y": 1
@@ -99,10 +84,10 @@ class TestSimulation():
             }
         })
         data['bodies'].append({
-            'mass': '1',
+            'mass': 1,
             "init_pos": {
                 "x": -1,
-                "y": -11
+                "y": -1
             },
             "init_vel": {
                 "x": -1,
@@ -110,8 +95,8 @@ class TestSimulation():
             }
         })
 
-        testfile = 'invisoplanet_detection/simulation/tests/testdata.json'
-        testout = 'invisoplanet_detection/simulation/tests/testoutput.txt'
+        testfile = 'testdata.json'
+        testout = 'testoutput.txt'
 
         with open(testfile, 'w') as f:
             json.dump(data, f)
@@ -128,11 +113,13 @@ class TestSimulation():
         nt.assert_equal(np.any(np.isnan(mf)), False)
 
         # make sure none of the bodies are stacked on top of each other
-        distances = []
-        for bodyi in space_f.bodies:
-            for bodyj in space_f.bodies:
-                distances.extend(bodyi.scalardistance(bodyj))
-        nt.assert_greater(np.all(distances), 0.000001)
+        # distances = []
+        # for bodyi in space_f.bodies:
+        #     for bodyj in space_f.bodies:
+        #         distances.extend([bodyi.scalardistance(bodyj)])
+        # disttest = np.where(distances > 1e-6)
+        # nt.assert_greater(disttest, True)
+        nt.assert_greater(space_f.bodies[0].scalardistance(space_f.bodies[1]), 1e-6)
 
         # make sure none of the masses have changed
         for i in range(2):
@@ -152,7 +139,7 @@ class TestSimulation():
             x, v, m = space.arrayvals()
             kinetic = np.sum(0.5 * v**2 * m)
 
-            potential = - space.G * np.product(m) / space.bodies[0].scalardistance(space.bodies[1])
+            potential = - space.bodies[0].G * np.product(m) / space.bodies[0].scalardistance(space.bodies[1])
             return kinetic + potential
 
         nt.assert_almost_equal(totalenergy(space_i), totalenergy(space_f))
@@ -162,7 +149,10 @@ class TestSimulation():
             x, v, m = space.arrayvals()
             return np.cross(x, m*v)
 
-        nt.assert_almost_equal(angularmomentum(space_i), angularmomentum(space_f))
+        Li = angularmomentum(space_i)
+        Lf = angularmomentum(space_f)
+        for i in range(2):
+            nt.assert_almost_equal(Li[i], Lf[i])
 
 
     def test_simulate(self):
