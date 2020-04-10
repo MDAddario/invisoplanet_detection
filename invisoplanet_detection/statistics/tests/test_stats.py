84250cf25cd21c62642fba86b5980412e328ae9e
MDAddario <michael.lindner.daddario@gmail.com>
2020-04-10 18:52:05 -0400

    Includes file for unit testing the stats module

---
+++ invisoplanet_detection/statistics/tests/test_stats.py
@@ -0,0 +1,17 @@
+import unittest
+import nose.tools as nt
+from invisoplanet_detection.statistics.stats import *
+
+
+class TestStats:
+
+
+	def test_initialize(self):
+		pass
+
+
+
+
+if __name__ == '__main__':
+
+	unittest.main()
