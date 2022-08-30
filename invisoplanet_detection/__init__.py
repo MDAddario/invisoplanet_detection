17660fbbe959b1ed4438e2b0597c10009b94791f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-30 14:06:08 +0800

    Delete invisoplanet_detection directory

--- invisoplanet_detection/__init__.py
+++
@@ -1,5 +0,0 @@
-# Import all of the submodules to invisoplanet_detection
-# If a new submodule is created, it needs to be added here
-from .statistics import *
-from .simulations import *
-from .animations import *
